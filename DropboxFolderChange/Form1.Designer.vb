0f230c73762f147bce77368bb63bab08c931a5d1
Enric Doblas <enrikk49@gmail.com>
2016-05-22 17:31:53 +0200

    Detecting change of serveral files in progress

--- DropboxFolderChange/Form1.Designer.vb
+++ DropboxFolderChange/Form1.Designer.vb
@@ -22,9 +22,180 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.PathFile = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PathDropbox = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DateModified = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.PathFile, Me.PathDropbox, Me.DateModified})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 5
+        Me.DataGridView1.Size = New System.Drawing.Size(510, 148)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 166)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "New rule"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 166)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Modify"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(174, 166)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 219)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(236, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 268)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(237, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 203)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Select Path File:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 252)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(138, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Select Path File in Dropbox:"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(255, 219)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Browse File"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(255, 265)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 9
+        Me.Button5.Text = "Browse File"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(359, 219)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(96, 69)
+        Me.Button6.TabIndex = 10
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'PathFile
+        '
+        Me.PathFile.HeaderText = "Path File"
+        Me.PathFile.Name = "PathFile"
+        Me.PathFile.ReadOnly = True
+        Me.PathFile.Width = 200
+        '
+        'PathDropbox
+        '
+        Me.PathDropbox.HeaderText = "Path File in Dropbox"
+        Me.PathDropbox.Name = "PathDropbox"
+        Me.PathDropbox.ReadOnly = True
+        Me.PathDropbox.Width = 200
+        '
+        'DateModified
+        '
+        Me.DateModified.HeaderText = "Last Date Modified"
+        Me.DateModified.Name = "DateModified"
+        Me.DateModified.ReadOnly = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(531, 348)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents PathFile As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents PathDropbox As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DateModified As System.Windows.Forms.DataGridViewTextBoxColumn
 
 End Class
--- DropboxFolderChange/Form1.vb
+++ DropboxFolderChange/Form1.vb
@@ -1,3 +1,174 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Dim fileConf As String = "C:\prova.txt"
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Load configuration
+        loadConf()
+        visibilityForm(False)
+        Run()
+        DataGridView1.Rows(0).Selected = True
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Button6.Text = "Add new rule"
+        textBrowser("", "")
+        visibilityForm(True)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim row As Integer = DataGridView1.CurrentCell.RowIndex
+        If (row >= 0) Then
+            Button6.Text = "Modify rule"
+            textBrowser(DataGridView1.Rows(row).Cells(0).Value, DataGridView1.Rows(row).Cells(1).Value)
+            visibilityForm(True)
+        Else
+            MsgBox("Select row to modify.")
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim row As Integer = DataGridView1.CurrentCell.RowIndex
+        visibilityForm(False)
+        If (row >= 0) Then
+            Dim answer As Integer = MsgBox("Are you sure you want to delete the rule?", vbYesNo + vbQuestion, "Empty Sheet")
+            If answer = vbYes Then
+                DataGridView1.Rows.RemoveAt(row)
+                modifyFile()
+            End If
+        Else
+            MsgBox("Select row to delete.")
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        browseFile(TextBox1)
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        browseFile(TextBox2)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If validateForm() Then
+            If Button6.Text = "Modify rule" Then
+                Dim row As Integer = DataGridView1.CurrentCell.RowIndex
+                DataGridView1.Rows(row).Cells(0).Value() = TextBox1.Text
+                DataGridView1.Rows(row).Cells(1).Value() = TextBox2.Text
+                modifyFile()
+            Else
+                Dim str As String = TextBox1.Text & ";" & TextBox2.Text & ";" & Date.Today
+                cutStr(str)
+                newLine(str)
+            End If
+            visibilityForm(False)
+        End If
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        DataGridView1.Rows(e.RowIndex).Selected = True
+    End Sub
+
+    Private Shared Sub Run()
+
+        Dim args() As String = System.Environment.GetCommandLineArgs()
+
+        ' Create a new FileSystemWatcher and set its properties.
+        Dim watcher As New FileSystemWatcher()
+        watcher.Path = "C:\"
+        ' Watch for changes in LastAccess and LastWrite times, and
+        ' the renaming of files or directories. 
+        watcher.NotifyFilter = (NotifyFilters.LastAccess Or NotifyFilters.LastWrite Or NotifyFilters.FileName Or NotifyFilters.DirectoryName)
+
+        ' Add event handlers.
+        AddHandler watcher.Changed, AddressOf OnChanged
+
+        ' Begin watching.
+        watcher.EnableRaisingEvents = True
+
+    End Sub
+
+    ' Define the event handlers.
+    Private Shared Sub OnChanged(source As Object, e As FileSystemEventArgs)
+        ' Specify what is done when a file is changed, created, or deleted.
+        MsgBox("Ha cambiat el fitxer " & e.FullPath)
+    End Sub
+
+    Private Sub loadConf()
+        Try
+            Dim sr As New StreamReader(fileConf)
+            While sr.Peek() >= 0
+                cutStr(sr.ReadLine())
+            End While
+
+            sr.Close()
+        Catch E As Exception
+            Console.WriteLine("The file could not be read:")
+            Console.WriteLine(E.Message)
+        End Try
+    End Sub
+
+    Private Sub cutStr(line As String)
+        Dim delimiter As String = ";"
+        DataGridView1.Rows.Add(line.Split(delimiter.ToCharArray()))
+    End Sub
+
+    Private Sub browseFile(textB As TextBox)
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+            textB.Text = OpenFileDialog1.FileName
+        End If
+
+    End Sub
+
+    Private Sub visibilityForm(state As Boolean)
+        Label1.Visible = state
+        Label2.Visible = state
+        TextBox1.Visible = state
+        TextBox2.Visible = state
+        Button4.Visible = state
+        Button5.Visible = state
+        Button6.Visible = state
+    End Sub
+
+    Private Sub textBrowser(strText1 As String, strText2 As String)
+        TextBox1.Text = strText1
+        TextBox2.Text = strText2
+    End Sub
+
+    Private Function validateForm() As Boolean
+
+        If TextBox1.Text = "" Or TextBox2.Text = "" Then
+            MsgBox("The file/s isn't selected")
+            Return False
+        Else
+            Dim Formato As String = "^[A-Z]:\\.*$"
+            If My.Computer.FileSystem.FileExists(TextBox1.Text) And My.Computer.FileSystem.FileExists(TextBox2.Text) Then
+                Return True
+            Else
+                MsgBox("Use a button Browse")
+                Return False
+            End If
+        End If
+    End Function
+
+    Private Sub modifyFile()
+        Dim TheFileLines As New List(Of String)
+        For i As Integer = 0 To DataGridView1.Rows.Count - 2
+            TheFileLines.Add(DataGridView1.Rows(i).Cells(0).Value() & ";" & DataGridView1.Rows(i).Cells(1).Value() & ";" & Date.Today)
+        Next
+        System.IO.File.WriteAllLines(fileConf, TheFileLines.ToArray)
+
+    End Sub
+
+    Private Sub newLine(str As String)
+        Dim file As System.IO.StreamWriter
+        file = My.Computer.FileSystem.OpenTextFileWriter(fileConf, True)
+        file.WriteLine(str)
+        file.Close()
+    End Sub
 
 End Class
